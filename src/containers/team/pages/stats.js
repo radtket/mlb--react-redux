f79e4da88b18f837e32a0b5b7cafd4939362a646
Taylor Radtke <taylorradtke@gmail.com>
2019-03-02 19:34:33 -0500

    added players stats table

--- src/containers/team/pages/stats.js
+++ src/containers/team/pages/stats.js
@@ -38,11 +38,85 @@ class PageTeamStats extends Component {
       return <div>Loading...</div>;
     }
 
+    // console.log(this.splitStatsByPosition(teamStats));
+
+    // console.log(teamStats);
+
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-8">
+          <div className="col-sm-12">
             <h1>PageTeamStats</h1>
+            <table className="table table--responsive">
+              <thead>
+                <tr>
+                  <th>NAME</th>
+                  <th>GP</th>
+                  <th>AB</th>
+                  <th>R</th>
+                  <th>H</th>
+                  <th>2B</th>
+                  <th>3B</th>
+                  <th>HR</th>
+                  <th>RBI</th>
+                  <th>TB</th>
+                  <th>BB</th>
+                  <th>SO</th>
+                  <th>SB</th>
+                  <th>BA</th>
+                  <th>OBP</th>
+                  <th>SLG</th>
+                  <th>OPS</th>
+                </tr>
+              </thead>
+              <tbody>
+                {teamStats &&
+                  teamStats.map(player => {
+                    const {
+                      Name,
+                      Games,
+                      StatID,
+                      PlayerID,
+                      AtBats,
+                      Runs,
+                      Hits,
+                      Doubles,
+                      Triples,
+                      HomeRuns,
+                      RunsBattedIn,
+                      TotalBases,
+                      Walks,
+                      Strikeouts,
+                      StolenBases,
+                      BattingAverage,
+                      OnBasePercentage,
+                      SluggingPercentage,
+                      OnBasePlusSlugging,
+                    } = player;
+                    return (
+                      <tr key={`${StatID} ${PlayerID}`}>
+                        <td>{Name}</td>
+                        <td>{Games}</td>
+                        <td>{AtBats}</td>
+                        <td>{Runs}</td>
+                        <td>{Hits}</td>
+                        <td>{Doubles}</td>
+                        <td>{Triples}</td>
+                        <td>{HomeRuns}</td>
+                        <td>{RunsBattedIn}</td>
+                        <td>{TotalBases}</td>
+                        <td>{Walks}</td>
+                        <td>{Strikeouts}</td>
+                        <td>{StolenBases}</td>
+                        <td>{BattingAverage}</td>
+                        <td>{OnBasePercentage}</td>
+                        <td>{SluggingPercentage}</td>
+                        <td>{OnBasePlusSlugging}</td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
