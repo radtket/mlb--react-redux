d71023b4fe9c89faf82f2e2cc1b867b626444769
Taylor Radtke <taylorradtke@gmail.com>
2019-03-04 20:55:08 -0500

    consilatated box-shadows

--- src/containers/standings/components/DivisionComponent.js
+++ src/containers/standings/components/DivisionComponent.js
@@ -2,31 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const StandingsDivision = ({ division, divisionTeams }) => (
-  <table
-    className="table table--standings"
-    style={{
-      margin: "24px auto",
-      boxShadow:
-        "0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
-    }}>
-    <thead>
-      <tr>
-        <th>{division}</th>
-        <th>W</th>
-        <th>L</th>
-        <th>PCT</th>
-        <th>GB</th>
-        <th className="standings__sec-stat">Home</th>
-        <th className="standings__sec-stat">Road</th>
-      </tr>
-    </thead>
-    <tbody>{divisionTeams}</tbody>
-  </table>
+	<table
+		className="table table--standings"
+		style={{
+			margin: "24px auto",
+		}}>
+		<thead>
+			<tr>
+				<th>{division}</th>
+				<th>W</th>
+				<th>L</th>
+				<th>PCT</th>
+				<th>GB</th>
+				<th className="standings__sec-stat">Home</th>
+				<th className="standings__sec-stat">Road</th>
+			</tr>
+		</thead>
+		<tbody>{divisionTeams}</tbody>
+	</table>
 );
 
 StandingsDivision.propTypes = {
-  division: PropTypes.string.isRequired,
-  divisionTeams: PropTypes.arrayOf(PropTypes.object).isRequired,
+	division: PropTypes.string.isRequired,
+	divisionTeams: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default StandingsDivision;
--- src/containers/team/components/TeamStandings/index.js
+++ src/containers/team/components/TeamStandings/index.js
@@ -6,118 +6,122 @@ import { largestToSmallest, getLeagueName } from "../../../../utils/helpers";
 import Tabs from "../../../../components/Tabs/Tabs";
 
 class TeamStandings extends Component {
-  createStandingsTable = (standings, activeTeamObj, division = false) => {
-    return standings.reduce((divisionTeams, team) => {
-      const {
-        League: ActiveLeague,
-        Division: ActiveDivision,
-        Key: ActiveKey,
-      } = activeTeamObj;
-      const {
-        League: TeamLeague,
-        Division: TeamDivision,
-        Key: TeamKey,
-        City,
-        Name,
-        Wins,
-        Losses,
-        Percentage,
-        GamesBehind,
-      } = team;
+	createStandingsTable = (standings, activeTeamObj, division = false) => {
+		return standings.reduce((divisionTeams, team) => {
+			const {
+				League: ActiveLeague,
+				Division: ActiveDivision,
+				Key: ActiveKey,
+			} = activeTeamObj;
+			const {
+				League: TeamLeague,
+				Division: TeamDivision,
+				Key: TeamKey,
+				City,
+				Name,
+				Wins,
+				Losses,
+				Percentage,
+				GamesBehind,
+			} = team;
 
-      if (division) {
-        TeamLeague === ActiveLeague &&
-          TeamDivision === ActiveDivision &&
-          divisionTeams.push(
-            <StandingsTeam
-              activeTeam={TeamKey === ActiveKey}
-              key={TeamKey}
-              City={City}
-              GamesBehind={GamesBehind}
-              Losses={Losses}
-              TeamKey={TeamKey}
-              TeamName={Name}
-              Wins={Wins}
-            />
-          );
-      } else {
-        TeamLeague === ActiveLeague &&
-          divisionTeams.push(
-            <StandingsTeam
-              activeTeam={TeamKey === ActiveKey}
-              key={TeamKey}
-              Losses={Losses}
-              City={City}
-              Percentage={Percentage.toFixed(3).replace(/^0+/, "")}
-              TeamKey={TeamKey}
-              TeamName={Name}
-              Wins={Wins}
-            />
-          );
-      }
-      return divisionTeams;
-    }, []);
-  };
+			if (division) {
+				TeamLeague === ActiveLeague &&
+					TeamDivision === ActiveDivision &&
+					divisionTeams.push(
+						<StandingsTeam
+							activeTeam={TeamKey === ActiveKey}
+							key={TeamKey}
+							City={City}
+							GamesBehind={GamesBehind}
+							Losses={Losses}
+							TeamKey={TeamKey}
+							TeamName={Name}
+							Wins={Wins}
+						/>
+					);
+			} else {
+				TeamLeague === ActiveLeague &&
+					divisionTeams.push(
+						<StandingsTeam
+							activeTeam={TeamKey === ActiveKey}
+							key={TeamKey}
+							Losses={Losses}
+							City={City}
+							Percentage={Percentage.toFixed(3).replace(/^0+/, "")}
+							TeamKey={TeamKey}
+							TeamName={Name}
+							Wins={Wins}
+						/>
+					);
+			}
+			return divisionTeams;
+		}, []);
+	};
 
-  render() {
-    const { standings, activeTeamObj } = this.props;
-    const { Division, League } = activeTeamObj;
-    return (
-      <div className="card">
-        <h5 className="card__headline">Standings</h5>
-        <Tabs>
-          <div label="Division">
-            <table className="table table--standings">
-              <thead>
-                <tr>
-                  <th>{`${League} ${Division}`}</th>
-                  <th>W</th>
-                  <th>L</th>
-                  <th>GB</th>
-                </tr>
-              </thead>
-              <tbody>
-                {standings &&
-                  this.createStandingsTable(standings, activeTeamObj, true)}
-              </tbody>
-            </table>
-          </div>
-          <div label="League">
-            <table className="table table--standings">
-              <thead>
-                <tr>
-                  <th>{getLeagueName(League)}</th>
-                  <th>W</th>
-                  <th>L</th>
-                  <th>PCT</th>
-                </tr>
-              </thead>
-              <tbody>
-                {standings &&
-                  this.createStandingsTable(
-                    standings.sort(largestToSmallest("Percentage")),
-                    activeTeamObj
-                  )}
-              </tbody>
-            </table>
-          </div>
-        </Tabs>
-        <footer className="card__footer">
-          <Link className="text-uppercase" to="/standings">
-            Full Standings
-          </Link>
-        </footer>
-      </div>
-    );
-  }
+	render() {
+		const { standings, activeTeamObj } = this.props;
+		const { Division, League } = activeTeamObj;
+		return (
+			<div className="card">
+				<h5 className="card__headline">Standings</h5>
+				<Tabs>
+					<div label="Division">
+						<table
+							className="table table--standings"
+							style={{
+								boxShadow: "none",
+							}}>
+							<thead>
+								<tr>
+									<th>{`${League} ${Division}`}</th>
+									<th>W</th>
+									<th>L</th>
+									<th>GB</th>
+								</tr>
+							</thead>
+							<tbody>
+								{standings &&
+									this.createStandingsTable(standings, activeTeamObj, true)}
+							</tbody>
+						</table>
+					</div>
+					<div label="League">
+						<table className="table table--standings">
+							<thead>
+								<tr>
+									<th>{getLeagueName(League)}</th>
+									<th>W</th>
+									<th>L</th>
+									<th>PCT</th>
+								</tr>
+							</thead>
+							<tbody>
+								{standings &&
+									this.createStandingsTable(
+										standings.sort(largestToSmallest("Percentage")),
+										activeTeamObj
+									)}
+							</tbody>
+						</table>
+					</div>
+				</Tabs>
+				<footer className="card__footer">
+					<Link className="text-uppercase" to="/standings">
+						Full Standings
+					</Link>
+				</footer>
+			</div>
+		);
+	}
 }
 
 TeamStandings.propTypes = {
-  standings: PropTypes.arrayOf(PropTypes.object).isRequired,
-  activeTeamObj: PropTypes.shape({
-    League: PropTypes.string.isRequired,
-    Key: PropTypes.string.isRequired,
-  }).isRequired,
+	standings: PropTypes.arrayOf(PropTypes.object).isRequired,
+	activeTeamObj: PropTypes.shape({
+		League: PropTypes.string.isRequired,
+		Key: PropTypes.string.isRequired,
+	}).isRequired,
 };
 
 export default TeamStandings;
