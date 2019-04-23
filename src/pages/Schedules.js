358771902f83bea1b2007c7a240ddfd30b6b45dd
Taylor Radtke <taylorradtke@gmail.com>
2019-04-22 21:00:54 -0400

    finished logic for schedule

--- src/pages/Schedules.js
+++ src/pages/Schedules.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -9,19 +10,16 @@ import SingleGame from "../components/Standings/SingleGame";
 import { TodaysDate, isArrayEmpty, isObjectEmpty } from "../utils/helpers";
 import LoadingSpinner from "../components/LoadingSpinner";
 
+import "../assets/scss/components/slick-theme.scss";
+
 const SchedulesList = ({ schedulesError, schedulesLoading, schedules }) => {
   const slider = useRef();
+
   const [activeTab, setActiveTab] = useState(TodaysDate);
-  const [organizedGames, setOrganizedGames] = useState([]);
+  const [activeGames, setActiveGames] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
   const [startAndEndDay, setStartAndEndDay] = useState({});
-
-  if (schedulesError) {
-    return <div>Error! {schedulesError.message}</div>;
-  }
-
-  if (schedulesLoading) {
-    return <LoadingSpinner />;
-  }
+  const [activeDisplayDate, setActiveDisplayDate] = useState(null);
 
   const getStartAndEndDays = () => {
     console.log("ran getStartAndEndDays");
@@ -35,6 +33,7 @@ const SchedulesList = ({ schedulesError, schedulesLoading, schedules }) => {
   };
 
   const buildEmptyCalender = startAndEndDayArg => {
+    console.log("buildEmptyCalender");
     const { firstDay, lastDay } = startAndEndDayArg;
     let day = firstDay;
     const allDays = {};
@@ -48,10 +47,12 @@ const SchedulesList = ({ schedulesError, schedulesLoading, schedules }) => {
     return allDays;
   };
 
-  const buildSomethingElse = emptyCalender => {
+  const buildGamesCalander = emptyCalender => {
+    console.log("buildGamesCalander");
     return Object.entries(
-      schedules.reduce((allGames, game) => {
+      schedules.reduce((allDates, game) => {
         const Day = dateFns.format(new Date(game.Day), "YYYY-MM-DD");
+        const allGames = allDates;
         allGames[Day] = allGames[Day] || [];
         allGames[Day].push(game);
         return allGames;
@@ -77,76 +78,104 @@ const SchedulesList = ({ schedulesError, schedulesLoading, schedules }) => {
     });
   };
 
-  // const buildIt = () => {
-  //   return Object.entries(
-  //     schedules.reduce((allGames, game) => {
-  //       const Day = dateFns.format(new Date(game.Day), "YYYY-MM-DD");
-  //       allGames[Day] = allGames[Day] || [];
-  //       allGames[Day].push(game);
-  //       return allGames;
-  //     }, {})
-  //   ).map(day => {
-  //     const [GameDate, Games] = day;
-  //     return (
-  //       <div key={GameDate} label={GameDate}>
-  //         {Games.map(game => (
-  //           <SingleGame key={game.GameID} {...game} />
-  //         ))}
-  //       </div>
-  //     );
-  //   });
-  // };
+  const getInitalActiveIndex = () => {
+    console.log("getInitalActiveIndex");
+    return activeGames.findIndex(child => {
+      const { label } = child.props;
+      const activeDayStartOfWeek = dateFns.format(
+        dateFns.startOfWeek(activeTab),
+        "YYYY-MM-DD"
+      );
+      return activeDayStartOfWeek === label;
+    });
+  };
 
   useEffect(() => {
     console.log("ran useEffect");
-    setStartAndEndDay(getStartAndEndDays());
-    // setOrganizedGames(buildIt());
-    setOrganizedGames(buildSomethingElse(buildEmptyCalender(startAndEndDay)));
-  }, []);
+    isObjectEmpty(startAndEndDay) && setStartAndEndDay(getStartAndEndDays());
+    isArrayEmpty(activeGames) &&
+      !isObjectEmpty(startAndEndDay) &&
+      setActiveGames(buildGamesCalander(buildEmptyCalender(startAndEndDay)));
 
-  if (isArrayEmpty(organizedGames) || isObjectEmpty(startAndEndDay)) {
-    return <h1>Loading</h1>;
+    !isArrayEmpty(activeGames) &&
+      !isObjectEmpty(startAndEndDay) &&
+      activeIndex === null &&
+      setActiveIndex(getInitalActiveIndex());
+    !isArrayEmpty(activeGames) &&
+      !isObjectEmpty(startAndEndDay) &&
+      activeDisplayDate === null &&
+      activeIndex &&
+      setActiveDisplayDate(activeIndex);
+  });
+
+  const buildNav = () => {
+    console.log("buildNav");
+    return activeGames.map(child => {
+      const { label } = child.props;
+
+      return (
+        <button
+          key={label}
+          className={`tabs-item ${activeTab === label ? "is-selected" : ""}`}
+          onClick={() => setActiveTab(label)}
+          type="button">
+          {label}
+        </button>
+      );
+    });
+  };
+
+  const buildBody = () => {
+    console.log("buildBody");
+    return activeGames.map(child => {
+      const { children: kids, label } = child.props;
+      if (label !== activeTab) {
+        return undefined;
+      }
+      return kids;
+    });
+  };
+
+  const renderVisibleStartAndStopDays = dayIndex => {
+    const day = activeGames[dayIndex] && activeGames[dayIndex].key;
+    const weekStart = dateFns.format(dateFns.startOfWeek(day), "YYYY-MM-DD");
+    const weekEnd = dateFns.format(dateFns.endOfWeek(day), "YYYY-MM-DD");
+    return `${weekStart} - ${weekEnd}`;
+  };
+
+  if (schedulesError) {
+    return <div>Error! {schedulesError.message}</div>;
   }
 
-  console.log(buildSomethingElse(buildEmptyCalender(startAndEndDay)));
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 7,
-    slidesToScroll: 7,
-    arrows: false,
-  };
+  if (
+    schedulesLoading ||
+    isObjectEmpty(startAndEndDay) ||
+    isArrayEmpty(activeGames) ||
+    activeIndex === null
+  ) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <div>
       <h1>Todays Games</h1>
-
-      <div className="tabs">
-        <nav>
-          {buildSomethingElse(buildEmptyCalender(startAndEndDay)).map(child => {
-            const { label } = child.props;
-            return (
-              <button
-                key={label}
-                className={`tabs-item ${
-                  activeTab === label ? "is-selected" : ""
-                }`}
-                onClick={() => setActiveTab(label)}
-                type="button">
-                {label}
-              </button>
-            );
-          })}
-        </nav>
-        <div className="tab-content">
-          {buildSomethingElse(buildEmptyCalender(startAndEndDay)).map(child => {
-            const { children: kids, label } = child.props;
-            if (label !== activeTab) return undefined;
-            return kids;
-          })}
-        </div>
+      <div className="col-sm-7">
+        <h1>{renderVisibleStartAndStopDays(activeDisplayDate)}</h1>
+        <Slider
+          ref={slider}
+          {...{
+            dots: true,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 7,
+            slidesToScroll: 7,
+            arrows: true,
+            initialSlide: activeIndex,
+            afterChange: current => setActiveDisplayDate(current),
+          }}>
+          {buildNav()}
+        </Slider>
+        <div className="tab-content">{buildBody()}</div>
       </div>
     </div>
   );
