3a9e556245c6d5724c3d67b1c8cdd865063bb5b7
Taylor Radtke <taylorradtke@gmail.com>
2024-12-07 17:02:37 -0600

    Updates

---
+++ precache-manifest.9ae66fb1a2643848fab33514c80174fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1550a38bae487c5a400be053d5f355d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306171763246e89d6b6f",
+    "url": "/static/css/2.0717822a.chunk.css"
+  },
+  {
+    "revision": "7328c1f2ef57b6019a47",
+    "url": "/static/css/main.906ec369.chunk.css"
+  },
+  {
+    "revision": "306171763246e89d6b6f",
+    "url": "/static/js/2.5afd3eb4.chunk.js"
+  },
+  {
+    "revision": "fd78dab20b5282cee97bca952f6839f6",
+    "url": "/static/js/2.5afd3eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7328c1f2ef57b6019a47",
+    "url": "/static/js/main.c1921fae.chunk.js"
+  },
+  {
+    "revision": "73416dd6530b94e42135",
+    "url": "/static/js/runtime-main.ad4c6c0a.js"
+  }
+]);
